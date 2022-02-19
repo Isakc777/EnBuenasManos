4c7963a6f6e0660e4cf4937eea406c3a003366fe
Oliver Sanchez Estrada <oliversancheze@gmail.com>
2022-02-18 21:36:19 -0500

    test3

--- app/src/main/java/com/example/enbuenasmanos/presentacion/LoginActivity.kt
+++ app/src/main/java/com/example/enbuenasmanos/presentacion/LoginActivity.kt
@@ -1,6 +1,7 @@
 
 package com.example.enbuenasmanos.presentacion
 //coment
+//comentario nuevo
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
