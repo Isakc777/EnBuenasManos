a77e553917d8a01e85396bb73f2dd559fcf564b9
Oliver Sanchez Estrada <oliversancheze@gmail.com>
2022-03-04 16:30:07 -0500

    conexion de login con firebase

--- app/src/main/java/com/example/enbuenasmanos/presentacion/LoginActivity.kt
+++ app/src/main/java/com/example/enbuenasmanos/presentacion/LoginActivity.kt
@@ -7,11 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.enbuenasmanos.MainActivity
-import com.example.enbuenasmanos.R
-import com.example.enbuenasmanos.controladores.UsuarioController
+import com.example.enbuenasmanos.ProviderType
 import com.example.enbuenasmanos.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -20,21 +20,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        validar()
+        sesion()
 
-        binding.btnLogin.setOnClickListener()
-        {
-            val access = UsuarioController().LoginUser(
-                binding.txtEmailAddress.text.toString(),
-                binding.txtPassword.text.toString()
-            )
-            if (access) {
-                binding.txtEmailAddress.error = getString(R.string.error)
-            } else {
-                binding.txtEmailAddress.error = null
-                var intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-        }
         binding.login.setOnClickListener() {
             hiddenIME(binding.root)
         }
@@ -45,6 +33,54 @@ class LoginActivity : AppCompatActivity() {
         val imm =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun validar(){
+        binding.btnLogin.setOnClickListener{
+            if (binding.txtEmailAddress.text.isNotEmpty() && binding.txtPassword.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.txtEmailAddress.text.toString(),
+                    binding.txtPassword.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email?:"", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+                }
+
+            }
+        }
+    }
+
+    //metodo para probar si existe una sesión activa
+    private fun sesion(){
+        val prefs = getSharedPreferences("com.example.pruebafirebase.PREFERENCE_FILE_KEY",MODE_PRIVATE)
+        val email= prefs.getString("email",null)
+        val provider = prefs.getString("provider",null)
+        val password = prefs.getString("password",null)
+        if(email!=null && provider!=null){
+            //binding.login.visibility = View.INVISIBLE
+            showHome(email,ProviderType.valueOf(provider))
+        }
+
+    }
+
+    private fun  showAlert (){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al Usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =  builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email:String,provider:ProviderType){
+        val password:String = binding.txtPassword.text.toString()
+        var intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email",email)
+            putExtra("provider",provider.name)
+            putExtra("password",password)
+        }
+        startActivity(intent)
     }
 
 
--- app/src/main/java/com/example/enbuenasmanos/presentacion/RegistroUsuarioActivity.kt
+++ app/src/main/java/com/example/enbuenasmanos/presentacion/RegistroUsuarioActivity.kt
@@ -6,7 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.enbuenasmanos.ProviderType
 import com.example.enbuenasmanos.databinding.ActivityRegistroUsuarioBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistroUsuarioActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistroUsuarioBinding
@@ -22,6 +26,7 @@ class RegistroUsuarioActivity : AppCompatActivity() {
             var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+        registrar()
         binding.registroUsuario.setOnClickListener() {
             hiddenIME(binding.root)
         }
@@ -34,12 +39,40 @@ class RegistroUsuarioActivity : AppCompatActivity() {
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    /*private fun saveFavNews(usuario: Usuario?) {
-        if (usuario != null) {
-                lifecycleScope.launch {
-                    UsuarioController().saveUsuario(usuario)
+    private fun registrar (){
+        binding.btnRegistrar.setOnClickListener(){
+            if (binding.txtEmailAddress.text.isNotEmpty() && binding.txtPassword.text.isNotEmpty()){
+                if(binding.txtPassword.text.toString() == binding.txtConfirmarPassword.text.toString()){
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.txtEmailAddress.text.toString(),
+                        binding.txtPassword.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            showHome(it.result?.user?.email?:"", ProviderType.BASIC)
+                            Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show()
+                        }else{
+                            showAlert()
+                        }
+                    }
+                }else{
+                    showAlert()
                 }
-            Toast.makeText(this, "registrado", Toast.LENGTH_SHORT).show()
+            }
         }
-    }*/
+    }
+
+    private fun showHome(email:String,provider:ProviderType){
+        var intent = Intent(this, LoginActivity::class.java).apply {
+            putExtra("email",email)
+            putExtra("provider",provider.name)
+        }
+        startActivity(intent)
+    }
+
+    private fun  showAlert (){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("La contraseñas no coinciden intente nuevamente")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =  builder.create()
+        dialog.show()
+    }
 }
