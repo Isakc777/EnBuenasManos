493deb1cbae01a8f66b844c4c262eb541a3c8871
Oliver Sanchez Estrada <oliversancheze@gmail.com>
2022-03-21 16:38:31 -0500

    eliminacion de archivos innecesarios

--- app/src/main/java/com/example/enbuenasmanos/entidades/Comunidad.kt
+++
@@ -1,3 +0,0 @@
-package com.example.enbuenasmanos.entidades
-
-data class Comunidad(val author: String, val title: String, val desc: String, val img: String)
--- app/src/main/java/com/example/enbuenasmanos/logica/ProfesionalBL.kt
+++
@@ -1,9 +0,0 @@
-package com.example.enbuenasmanos.logica
-
-import com.example.enbuenasmanos.Profesional
-import com.example.enbuenasmanos.casosUso.ComunidadUseCase
-import com.example.enbuenasmanos.entidades.Comunidad
-
-class ProfesionalBL {
-
-}
